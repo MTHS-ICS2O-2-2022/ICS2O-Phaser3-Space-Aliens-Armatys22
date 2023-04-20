9639efdaa6e690c932ebc72a0fb363df31d92903
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-04-20 00:16:13 +0000

    aded comments to our file

--- js/game.js
+++ js/game.js
@@ -1,9 +1,30 @@
 /* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: dom
+// Created on: Sep 2020
+// This is the phaser3 configuration file
+
+//* Game scene */
 const config = {
 type: Phaser.AUTO,
 width: 1920,
 height: 1080,
-backgroundColor: 0x5f6e7a
+physics: {
+  default: 'arcade',
+  arcade: {
+debug: true
+  }
+},
+// set background color
+backgroundColor: 0x5f6e7a,
+scale: {
+  mode: Phaser.Scale.FIT,
+  //WE place it in the middle of the page.
+  autoCentre: Phaser.Scale.CENTER_BOTH
 }
+}
+
 const game = new Phaser.Game(config)
 console.log(game)
